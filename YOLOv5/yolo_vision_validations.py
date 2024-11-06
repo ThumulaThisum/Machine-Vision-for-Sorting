6dd6d483016711eb2dfb3cfc6d7b1cfa437ddf23
Thumula Thisum <ttmpatabendige@gmail.com>
2024-11-06 06:28:16 +0200

    Add files via upload

---
+++ YOLOv5/yolo_vision_validations.py
@@ -0,0 +1,42 @@
+#packages 
+import pyniryo 
+from pyniryo2 import *
+from PIL import Image, ImageOps
+from YOLO_Detector import YOLO_Detector
+import cv2
+import time 
+import numpy as np
+import os 
+
+#connecting the robot
+client = NiryoRobot("192.168.0.101")
+client.arm.calibrate_auto()
+client.arm.move_joints([1.573, 0.081, -0.896, 0.080, -1.011, -0.109])
+
+#calibration parameters 
+mtx, dst = client.vision.get_camera_intrinsics()
+
+data_path = r"C:\Users\ttmpa\Desktop\od_models\yolov5\Notes\2_predictions\data.yaml"
+model_path = r"C:\Users\ttmpa\Desktop\od_models\yolov5\Notes\2_predictions\Model\weights\best.onnx"
+detector = YOLO_Detector(model_path, data_path)
+
+while True: 
+    #Getting the image from the robot 
+    img_compressed = client.vision.get_img_compressed()
+    img_raw = pyniryo.uncompress_image(img_compressed)
+    img_undistort = pyniryo.undistort_image(img_raw, mtx, dst)
+    img = img_undistort.copy()
+
+    #getting predicitions 
+    start_time = time.time()
+    preds, info = detector.predictions(img)
+    end_time =time.time()
+    inference_time = end_time - start_time
+
+    #showing images 
+    text = f"The inference time is {inference_time:.4} seconds"
+    cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2 )
+    cv2.imshow('Vision validation', preds)
+    if cv2.waitKey(0) == 27:
+        break
+    
